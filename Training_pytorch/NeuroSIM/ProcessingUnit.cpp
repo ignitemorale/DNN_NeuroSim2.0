cee6596a476153cda76ae27e1d416caa098d0b6f
ignitemorale <125619233+ignitemorale@users.noreply.github.com>
2023-03-02 10:09:42 +0900

    Update ProcessingUnit.cpp

--- Training_pytorch/NeuroSIM/ProcessingUnit.cpp
+++ Training_pytorch/NeuroSIM/ProcessingUnit.cpp
@@ -991,5 +991,6 @@ double GetWriteUpdateEstimation(SubArray *subArray, Technology& tech, MemCell& c
 	} else {   // SRAM
 		*writeDynamicEnergyArray = 0; // leave to subarray.cpp 
 	}
+	return 0;
 }
 
